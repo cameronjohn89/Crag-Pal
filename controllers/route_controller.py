6dc6e328105874bce17ead3b5a7ade45013342cc
Cameron Lawrence <cameronjohn89@LAPTOP-H0L3ALNE>
2023-08-02 23:46:31 +1000

    updated the route controller for creating a new route instance. Fixed errFixed error 500

--- controllers/route_controller.py
+++ controllers/route_controller.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from models.route import Route
 from models.climb import Climb
 from models.user import User
+from models.crag import Crag
 from database import db
 
 # Create the blueprint for the route controller
@@ -18,7 +19,7 @@ def create_route():
     user_id = data.get('user_id')
     name = data.get('name')
     difficulty_level = data.get('difficulty_level')
-    route_type = data.get('type')
+    route_type = data.get('route_type')
     description = data.get('description')
 
     # Check if the crag and user exist
@@ -28,8 +29,14 @@ def create_route():
         return jsonify({'error': 'Crag or user not found'}), 404
 
     # Create a new Route instance
-    route = Route(crag_id=crag_id, user_id=user_id, name=name, difficulty_level=difficulty_level,
-                  type=route_type, description=description)
+    route = Route(
+    crag_id=crag_id,
+    user_id=user_id,
+    name=name,
+    difficulty_level=difficulty_level,
+    route_type=route_type,
+    description=description
+)
 
     # Save the new route to the database
     db.session.add(route)
